7d2a74ca94842f81ba62053a04fa742088d92433
SonyaBlinova <sofa_blinova@mail.ru>
2021-12-18 13:31:42 +0100

    code refactoring

--- helpers.py
+++ helpers.py
@@ -2,7 +2,13 @@ import numpy as np
 import networkx as nx
 from tqdm import tqdm
 
+"""Numpy part"""
+
 def generate_graph_np(N, x_star, a, b):
+  """
+  Numpy version.
+  Observation graph generation. 
+  """
   graph = np.zeros((N, N))
   for i in range(N):
     for j in range(i+1, N):
@@ -17,6 +23,10 @@ def generate_graph_np(N, x_star, a, b):
   return graph
 
 def compute_h_np(graph, a, b, N):
+  """
+  Numpy version.
+  Compute h for the acceptance probabilities.
+  """
   ln1 = np.log(a/b)
   ln2 = np.log((1 - a/N)/(1 - b/N))
   h = np.zeros_like(graph)
@@ -24,6 +34,10 @@ def compute_h_np(graph, a, b, N):
   return h
 
 def compute_accep_prob_np(x, h, N, psi_ij, psi_ji, i):
+  """
+  Numpy version.
+  Compute acceptance probability.
+  """
   expo = 0
   for j in range(N):
     if j != i:
@@ -33,6 +47,10 @@ def compute_accep_prob_np(x, h, N, psi_ij, psi_ji, i):
   return a
 
 def hamiltonian_np(x, h, N):
+  """
+  Numpy version.
+  Hamiltonian.
+  """
   p = 0
   for i in range(N):
     for j in range(i+1, N):
@@ -40,12 +58,20 @@ def hamiltonian_np(x, h, N):
   return p
 
 def overlap_np(x_star, x_final, N):
+  """
+  Numpy version.
+  Overlap.
+  """
   over = 0
   for i in range(N):
     over += x_star[i]*x_final[i]
   return (1/N)*np.abs(over)
 
 def metropolis_step_np(curr_state, h, N, psi_ij, psi_ji):
+  """
+  Numpy version.
+  Metropolis step. It is used for the running metropolis algotith.
+  """
   rand_ind = np.random.choice(N, 1)[0]
   a = compute_accep_prob_np(curr_state, h, N, psi_ij, psi_ji, rand_ind)
   rand_num = np.random.uniform(0,1)
@@ -53,7 +79,22 @@ def metropolis_step_np(curr_state, h, N, psi_ij, psi_ji):
     curr_state[rand_ind] *= -1
   return curr_state
 
+def metropolis_run(curr_state, N, h, x_star, num_iter):
+  """
+  Numpy version.
+  Metroppolis algorithm.
+  """
+  for i in tqdm(range(num_iter)):
+    curr_state = metropolis_step_np(curr_state, h, N, 1/N, 1/N)
+  return overlap_np(x_star, curr_state, N)
+
+"""Networkx part"""
+
 def generate_graph(N, x_star, a, b):
+  """
+  Networkx version.
+  Observation graph deneration.
+  """
   graph = nx.Graph()
   for node in range(N):
     graph.add_node(node, cl=x_star[node])
@@ -68,6 +109,11 @@ def generate_graph(N, x_star, a, b):
   return graph
 
 def compute_h(graph, a, b, N):
+  """
+  Networkx version.
+  Compute h for the acceptance probabilities.
+
+  """
   graph = nx.to_numpy_matrix(graph)
   ln1 = np.log(a/b)
   ln2 = np.log((1 - a/N)/(1 - b/N))
@@ -76,6 +122,10 @@ def compute_h(graph, a, b, N):
   return h
 
 def compute_accep_prob(graph, h, N, psi_ij, psi_ji, i):
+  """
+  Networkx version.
+  Compute acceptance probability.
+  """
   expo = 0
   for j in range(N):
     if j != i:
@@ -85,6 +135,10 @@ def compute_accep_prob(graph, h, N, psi_ij, psi_ji, i):
   return a
 
 def hamiltonian(graph, h, N):
+  """
+  Networkx version.
+  Hamiltonian.
+  """
   p = 0
   for i in range(N):
     for j in range(i+1, N):
@@ -92,12 +146,20 @@ def hamiltonian(graph, h, N):
   return p
 
 def overlap(x_star, graph, N):
+  """
+  Networkx version.
+  Overlap.
+  """
   over = 0
   for i in range(N):
     over += x_star[i]*graph.nodes[i]['cl']
   return (1/N)*np.abs(over)
 
 def metropolis_step(graph, h, N, psi_1, psi_2):
+  """
+  Networkx version.
+  Metropolis step. It is used for the Houdayer algorithms.
+  """
   rand_ind = np.random.choice(N, 1)[0]
   a = compute_accep_prob(graph, h, N, psi_1, psi_2, rand_ind)
   rand_num = np.random.uniform(0,1)
@@ -106,29 +168,33 @@ def metropolis_step(graph, h, N, psi_1, psi_2):
   return graph
 
 def houdayer_step(G1, G2):
-  #create subgraph
+  """
+  Networkx version.
+  Houdayer step.
+  """
+  # create subgraph
   subgraph_nodes = [i[0] for i in set(nx.get_node_attributes(G1, "cl").items()) - set(nx.get_node_attributes(G2, "cl").items())]
   R = G1.subgraph(subgraph_nodes)
 
   if len(list(R.nodes)) != 0:
-    r_st = np.random.choice(list(R.nodes)) #choosing randomly element
+    # choosing randomly element
+    r_st = np.random.choice(list(R.nodes))
     G1.nodes[r_st]['cl'] *= -1
     G2.nodes[r_st]['cl'] *= -1
-    #finding all connections of the chosen element
+    # finding all connections of the chosen element
     for i in R.neighbors(r_st):
       G1.nodes[i]['cl'] *= -1
       G2.nodes[i]['cl'] *= -1
   return G1, G2
 
 def houdayer_run(graph_curr_1, graph_curr_2, N, h, x_star, num_iter, n0 = 1):
+  """
+  Networkx version.
+  Houdayer and Mixed algorithms.
+  """
   for i in tqdm(range(num_iter)):
     graph_curr_1 = metropolis_step(graph_curr_1, h, N, 1/N, 1/N)
     graph_curr_2 = metropolis_step(graph_curr_2, h, N, 1/N, 1/N)
     if i % n0 == 0:
       graph_curr_1, graph_curr_2 = houdayer_step(graph_curr_1, graph_curr_2)
   return overlap(x_star, graph_curr_1, N), overlap(x_star, graph_curr_2, N)
-
-def metropolis_run(curr_state, N, h, x_star, num_iter):
-  for i in tqdm(range(num_iter)):
-    curr_state = metropolis_step_np(curr_state, h, N, 1/N, 1/N)
-  return overlap_np(x_star, curr_state, N)
--- plotting.py
+++ plotting.py
@@ -4,6 +4,168 @@ from tqdm import tqdm
 from operator import add
 from helpers import *
 
+
+# base chain !!!!!!!!!!!
+
+# convergence time - ham + over all at one
+# plot dependece of the conv time with different r and d
+# dependence on b/a ration - 1) try different fixed a values for each algo. 1 plot per algorithm (pay attension on d) 2) choose best fixed value for each algo, compare them on the 1 plot
+# dependence over n0
+# dependence on b/a for mixed algo for different n0
+
+"""Plot helpers"""
+
+
+"""Convergence time"""
+
+# convergence time - ham + over all at one
+
+def plot_time_overlap_algorithms_compare(graph, N, a, b, x_star, num_iter, n0):
+  h = compute_h(graph, a, b, N)
+
+  hamiltonians_1 = []
+  hamiltonians_2 = []
+  hamiltonians_3 = []
+  
+  overlaps_1 = []
+  overlaps_2 = []
+  overlaps_3 = []
+
+  times = []
+  
+  curr_state = np.random.choice([-1, 1], N)
+  
+  graph_2_1 = graph.copy()
+  nx.set_node_attributes(graph_2_1, dict(zip(range(N), curr_state)), 'cl')
+  graph_2_2 = graph.copy()
+  nx.set_node_attributes(graph_2_2, dict(zip(range(N), curr_state)), 'cl')
+  
+  graph_3_1 = graph.copy()
+  nx.set_node_attributes(graph_3_1, dict(zip(range(N), curr_state)), 'cl')
+  graph_3_2 = graph.copy()
+  nx.set_node_attributes(graph_3_2, dict(zip(range(N), curr_state)), 'cl')
+
+  for i in range(num_iter):
+    curr_state = metropolis_step_np(curr_state, h, N, 1/N, 1/N)
+    if i % 2 == 0:
+      graph_2_1 = metropolis_step(graph_2_1, h, N, 1/N, 1/N)
+      graph_2_2 = metropolis_step(graph_2_2, h, N, 1/N, 1/N)
+    else:
+      graph_2_1, graph_2_2 = houdayer_step(graph_2_1, graph_2_2)
+
+    if i % n0 == 1:
+      graph_3_1, graph_3_2 = houdayer_step(graph_3_1, graph_3_2)
+    else:
+      graph_3_1 = metropolis_step(graph_3_1, h, N, 1/N, 1/N)
+      graph_3_2 = metropolis_step(graph_3_2, h, N, 1/N, 1/N)
+    
+
+    if i % 15 == 0:
+      hamiltonians_1.append(hamiltonian_np(curr_state, h, N))
+      hamiltonians_2.append(hamiltonian(graph_2_1, h, N))
+      hamiltonians_3.append(hamiltonian(graph_3_1, h, N))
+
+      overlaps_1.append(overlap_np(x_star, curr_state, N))
+      overlaps_2.append(overlap(x_star, graph_2_1, N))
+      overlaps_3.append(overlap(x_star, graph_3_1, N))
+      
+      times.append(i)
+  
+  return hamiltonians_1, hamiltonians_2, hamiltonians_3, overlaps_1, overlaps_2, overlaps_3, times
+
+# plot dependece of the conv time with different r and d
+
+"""b/a ratio"""
+
+# dependence on b/a ration - 1) try different fixed a values for each algo. 1 plot per algorithm (pay attension on d) 2) choose best fixed value for each algo, compare them on the 1 plot
+
+# dependence on b/a for mixed algo for different n0
+
+"""n0"""
+
+# dependence over n0
+
+def houdayer_time_plot_compare_n0_helper(hamiltonians_1, hamiltonians_2, hamiltonians_3, overlaps_1, overlaps_2, overlaps_3, times, N, a, b, n0_1, n0_2, n0_3):
+  _, axes = plt.subplots(nrows=1, ncols=1, figsize=(16,8))
+  axes.set_ylabel('Overlap', fontsize=18)
+  axes.set_xlabel('Time', fontsize=18)
+  axes.grid()
+  title = 'Overlap over Time: Mixed Metropolis-Houdayer Algorithm with N = ' + str(N) + ', a = ' + str(a) + ', b = ' + str(b)
+  axes.set_title(title, fontsize=15)
+  label_1 = "n0 = " + str(n0_1 - 1)
+  label_2 = "n0 = " + str(n0_2 - 1)
+  label_3 = "n0 = " + str(n0_3 - 1)
+  axes.plot(times, overlaps_1, label = label_1)
+  axes.plot(times, overlaps_2, label = label_2)
+  axes.plot(times, overlaps_3, label = label_3) 
+  axes.legend()
+  filename = 'plots/overlap_mixed_compare_n0_N' + str(N) + "a" + str(a) + "b" + str(b) + '.png'
+  plt.savefig(filename)
+  plt.show()
+
+def plot_time_overlap_mixed_compare(graph, N, a, b, x_star, num_iter, n0_1, n0_2, n0_3):
+  h = compute_h(graph, a, b, N)
+
+  hamiltonians_1 = []
+  hamiltonians_2 = []
+  hamiltonians_3 = []
+
+  overlaps_1 = []
+  overlaps_2 = []
+  overlaps_3 = []
+
+  times = []
+
+  initial_state = np.random.choice([-1, 1], N)
+
+  graph_1_1 = graph.copy()
+  nx.set_node_attributes(graph_1_1, dict(zip(range(N), initial_state)), 'cl')
+  graph_1_2 = graph_1_1.copy()
+  
+  graph_2_1 = graph_1_1.copy()
+  graph_2_2 = graph_1_1.copy()
+  
+  graph_3_1 = graph_1_1.copy()
+  graph_3_2 = graph_1_1.copy()
+
+
+
+  for i in range(num_iter):
+    if i % n0_1 == 1:
+      graph_1_1, graph_1_2 = houdayer_step(graph_1_1, graph_1_2)
+    else:
+      graph_1_1 = metropolis_step(graph_1_1, h, N, 1/N, 1/N)
+      graph_1_2 = metropolis_step(graph_1_2, h, N, 1/N, 1/N)
+
+    if i % n0_2 == 1:
+      graph_2_1, graph_2_2 = houdayer_step(graph_2_1, graph_2_2)
+    else:
+      graph_2_1 = metropolis_step(graph_2_1, h, N, 1/N, 1/N)
+      graph_2_2 = metropolis_step(graph_2_2, h, N, 1/N, 1/N)
+
+    if i % n0_3 == 1:
+      graph_3_1, graph_3_2 = houdayer_step(graph_3_1, graph_3_2)
+    else:
+      graph_3_1 = metropolis_step(graph_3_1, h, N, 1/N, 1/N)
+      graph_3_2 = metropolis_step(graph_3_2, h, N, 1/N, 1/N)
+    
+    if i % 10 == 0:
+      hamiltonians_1.append(hamiltonian(graph_1_1, h, N))
+      hamiltonians_2.append(hamiltonian(graph_2_1, h, N))
+      hamiltonians_3.append(hamiltonian(graph_3_1, h, N))
+
+      overlaps_1.append(overlap(x_star, graph_1_1, N))
+      overlaps_2.append(overlap(x_star, graph_2_1, N))
+      overlaps_3.append(overlap(x_star, graph_3_1, N))
+
+      times.append(i)
+
+      clear_output(wait=True)
+      
+      houdayer_time_plot_compare_n0_helper(hamiltonians_1, hamiltonians_2, hamiltonians_3, overlaps_1, overlaps_2, overlaps_3, times, N, a, b, n0_1, n0_2, n0_3)
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------
 def metropolis_time_plot_helper(hamiltonians, overlaps, times, N, a, b):
   _, axes = plt.subplots(nrows=1, ncols=2, figsize=(32,8))
   axes[0].set_ylabel('Hamiltonian', fontsize=18)
@@ -76,33 +238,6 @@ def houdayer_time_plot_compare_helper(hamiltonians_1, hamiltonians_2, hamiltonia
   plt.savefig(filename)
   plt.show()
 
-def houdayer_time_plot_compare_n0_helper(hamiltonians_1, hamiltonians_2, hamiltonians_3, overlaps_1, overlaps_2, overlaps_3, times, N, a, b, n0_1, n0_2, n0_3):
-  _, axes = plt.subplots(nrows=1, ncols=2, figsize=(32,8))
-  axes[0].set_ylabel('Hamiltonian', fontsize=18)
-  axes[0].set_xlabel('Time', fontsize=18)
-  axes[1].set_ylabel('Overlap', fontsize=18)
-  axes[1].set_xlabel('Time', fontsize=18)
-  axes[0].grid()
-  axes[1].grid()
-  title1 = 'Hamiltonian over Time: Mixed Metropolis-Houdayer Algorithm with N = ' + str(N) + ', a = ' + str(a) + ', b = ' + str(b)
-  title2 = 'Overlap over Time: Mixed Metropolis-Houdayer Algorithm with N = ' + str(N) + ', a = ' + str(a) + ', b = ' + str(b)
-  axes[0].set_title(title1, fontsize=20)
-  axes[1].set_title(title2, fontsize=20)
-  label_1 = "n0 = " + str(n0_1 - 1)
-  label_2 = "n0 = " + str(n0_2 - 1)
-  label_3 = "n0 = " + str(n0_3 - 1)
-  axes[0].plot(times, hamiltonians_1, label = label_1)
-  axes[0].plot(times, hamiltonians_2, label = label_2)
-  axes[0].plot(times, hamiltonians_3, label = label_3)
-  axes[1].plot(times, overlaps_1, label = label_1)
-  axes[1].plot(times, overlaps_2, label = label_2)
-  axes[1].plot(times, overlaps_3, label = label_3) 
-  axes[0].legend()
-  axes[1].legend()
-  filename = 'plots/overlap_mixed_compare_n0_N' + str(N) + "a" + str(a) + "b" + str(b) + '.png'
-  plt.savefig(filename)
-  plt.show()
-
 def plot_algorithms_compare_helper(hamiltonians_1, hamiltonians_2, hamiltonians_3, overlaps_1, overlaps_2, overlaps_3, times, N, a, b, n0):
   _, axes = plt.subplots(nrows=1, ncols=2, figsize=(32,8))
   axes[0].set_ylabel('Hamiltonian', fontsize=26)
@@ -166,6 +301,10 @@ def plot_ratio_houdayer_helper(ratio, over1, over2):
   plt.show()
 
 def plot_time_overlap_metropolis_compare(graph_1, graph_2, graph_3, N, a_1, b_1, a_2, b_2, a_3, b_3, x_star, num_iter):
+  """
+  Compare 3 different combination of a and b parameters.
+  Plot 3 hamiltonians and overlaps over time
+  """
   h_1 = compute_h_np(graph_1, a_1, b_1, N)
   h_2 = compute_h_np(graph_2, a_2, b_2, N)
   h_3 = compute_h_np(graph_3, a_3, b_3, N)
@@ -196,6 +335,9 @@ def plot_time_overlap_metropolis_compare(graph_1, graph_2, graph_3, N, a_1, b_1,
       metropolis_time_plot_compare_helper(hamiltonians_1, hamiltonians_2, hamiltonians_3, overlaps_1, overlaps_2, overlaps_3, times, N, a_1, b_1, a_2, b_2, a_3, b_3)
 
 def plot_time_overlap_metropolis(graph, N, a, b, x_star, num_iter):
+  '''
+  Plot hamiltonian and overlap for 1 metropolis experiment.
+  '''
   h = compute_h_np(graph, a, b, N)
 
   hamiltonians = []
@@ -212,6 +354,9 @@ def plot_time_overlap_metropolis(graph, N, a, b, x_star, num_iter):
   metropolis_time_plot_helper(hamiltonians, overlaps, times, N, a, b)
 
 def plot_time_overlap_houdayer_compare(graph_1, graph_2, graph_3, N, a_1, b_1, a_2, b_2, a_3, b_3, x_star, num_iter):
+  """
+  Compare 3 different a and b configurations for Houdayer algorithm.
+  """
   h_1 = compute_h(graph_1, a_1, b_1, N)
   h_2 = compute_h(graph_2, a_2, b_2, N)
   h_3 = compute_h(graph_3, a_3, b_3, N)
@@ -451,58 +596,7 @@ def plot_time_average_overlap_algorithms_compare(N, a, b, num_iter, n0, num_exp)
   return hamiltonians_avg_1, hamiltonians_avg_2, hamiltonians_avg_3, overlaps_avg_1, overlaps_avg_2, overlaps_avg_3, times_avg
 
 
-def plot_time_overlap_algorithms_compare(graph, N, a, b, x_star, num_iter, n0):
-  h = compute_h(graph, a, b, N)
 
-  hamiltonians_1 = []
-  hamiltonians_2 = []
-  hamiltonians_3 = []
-  
-  overlaps_1 = []
-  overlaps_2 = []
-  overlaps_3 = []
-
-  times = []
-  
-  curr_state = np.random.choice([-1, 1], N)
-  
-  graph_2_1 = graph.copy()
-  nx.set_node_attributes(graph_2_1, dict(zip(range(N), curr_state)), 'cl')
-  graph_2_2 = graph.copy()
-  nx.set_node_attributes(graph_2_2, dict(zip(range(N), curr_state)), 'cl')
-  
-  graph_3_1 = graph.copy()
-  nx.set_node_attributes(graph_3_1, dict(zip(range(N), curr_state)), 'cl')
-  graph_3_2 = graph.copy()
-  nx.set_node_attributes(graph_3_2, dict(zip(range(N), curr_state)), 'cl')
-
-  for i in range(num_iter):
-    curr_state = metropolis_step_np(curr_state, h, N, 1/N, 1/N)
-    if i % 2 == 0:
-      graph_2_1 = metropolis_step(graph_2_1, h, N, 1/N, 1/N)
-      graph_2_2 = metropolis_step(graph_2_2, h, N, 1/N, 1/N)
-    else:
-      graph_2_1, graph_2_2 = houdayer_step(graph_2_1, graph_2_2)
-
-    if i % n0 == 1:
-      graph_3_1, graph_3_2 = houdayer_step(graph_3_1, graph_3_2)
-    else:
-      graph_3_1 = metropolis_step(graph_3_1, h, N, 1/N, 1/N)
-      graph_3_2 = metropolis_step(graph_3_2, h, N, 1/N, 1/N)
-    
-
-    if i % 15 == 0:
-      hamiltonians_1.append(hamiltonian_np(curr_state, h, N))
-      hamiltonians_2.append(hamiltonian(graph_2_1, h, N))
-      hamiltonians_3.append(hamiltonian(graph_3_1, h, N))
-
-      overlaps_1.append(overlap_np(x_star, curr_state, N))
-      overlaps_2.append(overlap(x_star, graph_2_1, N))
-      overlaps_3.append(overlap(x_star, graph_3_1, N))
-      
-      times.append(i)
-  
-  return hamiltonians_1, hamiltonians_2, hamiltonians_3, overlaps_1, overlaps_2, overlaps_3, times
 
 def plot_time_overlap_metropolis(graph, N, a, b, x_star, num_iter):
   h = compute_h_np(graph, a, b, N)
